d47668e14bdb6bd8b2908431780da03049b89c2c
JoaoNuno96 <joaonuno1996@hotmail.com>
2024-09-16 17:59:27 +0100

    testing

--- src/main/news/news.jsx
+++ src/main/news/news.jsx
@@ -1,45 +1,46 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import "/src/main/news/news.style.css";
-import {news} from "/src/assets/mjs/news.mjs";
+// import "/src/main/news/news.style.css";
+// import {news} from "/src/assets/mjs/news.mjs";
 
 export const News = (state) => {
 
   var id = state.id;
 
-  var titulo = news[id].titulo;
-  var texto = news[id].texto;
-  var image = news[id].image;
-  var linkExterno = news[id].linkExterno;
+  // var titulo = news[id].titulo;
+  // var texto = news[id].texto;
+  // var image = news[id].image;
+  // var linkExterno = news[id].linkExterno;
 
 
     return (
-      <div>
-         {/* <!-- CONTAINER INICIO --> */}
-        <div id="containerFirst" className="container">
+      <h1>ola {id}</h1>
+      // <div>
+      //    {/* <!-- CONTAINER INICIO --> */}
+      //   <div id="containerFirst" className="container">
           
-          <div className="row">
-            <div className="col">
-              <div className="jumbotron">
-                <h1 className="display-4 text-white">{titulo}</h1>
-                <p className="lead text-white">{texto}</p>
-                <hr id="line" className="my-4" />
-                <p className="text-white">Para mais informações, clique na ligação abaixo</p>
-                <p className="lead mt-4">
-                  <a className="btn btn-outline-light btn-lg" href={linkExterno} target="_blank" role="button">Noticia</a>
-                  <a className="btn btn-outline-light" href="/" role="button">Voltar</a>
-                </p>
-              </div>
-            </div>
+      //     <div className="row">
+      //       <div className="col">
+      //         <div className="jumbotron">
+      //           <h1 className="display-4 text-white">{titulo}</h1>
+      //           <p className="lead text-white">{texto}</p>
+      //           <hr id="line" className="my-4" />
+      //           <p className="text-white">Para mais informações, clique na ligação abaixo</p>
+      //           <p className="lead mt-4">
+      //             <a className="btn btn-outline-light btn-lg" href={linkExterno} target="_blank" role="button">Noticia</a>
+      //             <a className="btn btn-outline-light" href="/" role="button">Voltar</a>
+      //           </p>
+      //         </div>
+      //       </div>
 
-            <div className="col">
-              <img src={image} alt="Imagem Noticia" height="300px" />
-            </div>
+      //       <div className="col">
+      //         <img src={image} alt="Imagem Noticia" height="300px" />
+      //       </div>
             
-          </div>
-        </div>
-        {/* <!-- CONTAINER INICIO --> */}
+      //     </div>
+      //   </div>
+      //   {/* <!-- CONTAINER INICIO --> */}
 
-      </div>  
+      // </div>  
     );
 };
